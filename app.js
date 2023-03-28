b816b5bbabe8749c17ead77e9eb6800de74e353a
Syed Muhammed Hassan Ali <syed007hassan@gmail.com>
2023-03-29 02:22:03 +0500

    QUERY ADDED

--- app.js
+++ app.js
@@ -6,17 +6,73 @@ const app = express();
 
 // buildSchema is a function that takes a string as an argument
 const schema = buildSchema(`
-  type Query {
-    description: String
-    price: Float
+
+ type Query{
+  products: [Product]
+  orders: [Order]
+ }
+
+ type Product{
+  id: ID!
+  description: String!
+  reviews: [Review]
+  price: Float!
+  category: String!
+  orders: [Order]
+ }
+
+ type Review{
+  rating: Int!
+  comment: String!
+ }
+
+ type Order{
+  date: String!
+  subtotal: Float!
+  items: [OrderItem]
+
+ }
+
+  type OrderItem{
+    product: Product!
+    quantity: Int!
   }
+
 `);
 
 // root is an object that contains all of the resolvers
 const root = {
-  description: "This is a great product!",
-  price: 2.99,
+  products: [
+    {
+      id: 1,
+      description: "A great product",
+      reviews: [
+        {
+          rating: 5,
+          comment: "This product is great",
+        },
+      ],
+      price: 9.99,
+      category: "Electronics",
+      orders: [
+        {
+          date: "2021-01-01",
+          subtotal: 9.99,
+          items: [
+            {
+              product: {
+                id: 1,
+              },
+              quantity: 1,
+            },
+          ],
+        },
+      ],
+    },
+  ],
 };
+
+//MAKE A QUERY TO THE GRAPHQL API
 
 app.use(
   "/graphql",
